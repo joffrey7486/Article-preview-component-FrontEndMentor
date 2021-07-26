5d68f8f2a9ca23bb27a05cb4c9c62cbe6b79670e
joffrey7486 <64081938+joffrey7486@users.noreply.github.com>
2021-07-26 14:48:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ btnShare.addEventListener('click', () => {
         btnShare.style.overflow = "visible"
         share.style.opacity = '1'
         btnShare.style.backgroundColor =  'hsl(217, 19%, 35%)'
-        btnShare.style.backgroundImage = 'url(/images/icon-share-active.svg)'
+        btnShare.style.backgroundImage = 'url(https://raw.githubusercontent.com/joffrey7486/Article-preview-component-FrontEndMentor/b52464c7596a96959318d0b6d0baa086ceaf7a2e/images/icon-share-active.svg)'
     } else {
         btnShare.classList.add('.inactive')
         btnShare.classList.remove('.active')
         btnShare.style.overflow = "hidden"
         share.style.opacity = '0'
         btnShare.style.backgroundColor =  '#edf1fa'
-        btnShare.style.backgroundImage = 'url(/images/icon-share-inactive.svg)'
+        btnShare.style.backgroundImage = 'url(https://raw.githubusercontent.com/joffrey7486/Article-preview-component-FrontEndMentor/4223a81a4037dc37b82217a2e0ea9a1dcacad581/images/icon-share-inactive.svg)'
     }
 })
